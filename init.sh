7a260b1342ebe03ae036e74788ead47c0f148271
danadrianmirea <152083571+danadrianmirea@users.noreply.github.com>
2024-09-27 12:29:49 +0300

    Delete init.sh

--- init.sh
+++
@@ -1,7 +0,0 @@
-echo "# wsl_test" >> README.md
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:danadrianmirea/wsl_test.git
-git push -u origin main
